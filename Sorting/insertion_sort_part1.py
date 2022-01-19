9ee1cd0279b215cbe8d886d7ccbdf13d1664912a
solomonsh <s.seshiferaw@gmail.com>
2022-01-19 19:07:48 +0100

    insertion-sort

--- Sorting/insertion_sort_part1.py
+++ Sorting/insertion_sort_part1.py
@@ -20,4 +20,28 @@ def insertionSort1(n, arr):
     ars = " ".join(str(a) for a in arr)
     print(ars)
 
-# insertionSort1(10,[2,3,4,5,6,7,8,9,10,1])
+
+# Complete insertion sort
+# Time complexity N^2
+# Space complexity N
+
+def insertionSort(arr):
+
+    for i in range(1, len(arr)):
+        if arr[i-1] > arr[i]:
+
+            j = i
+            while j > 0:
+                if arr[j-1] <= arr[j]:
+                    break
+                else:
+                    arr[j], arr[j-1] = arr[j-1], arr[j]
+
+                j -= 1
+
+    return arr
+
+
+print(insertionSort([2, 4, -5, 6, 7, -8, 9, 10, 1]))
+
+print(insertionSort([0, 2, -4]))
