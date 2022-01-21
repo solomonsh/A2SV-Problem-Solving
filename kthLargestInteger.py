b6a0b6c66486de6428277bcf3c3eba1c79cab1a8
solomonsh <s.seshiferaw@gmail.com>
2022-01-21 19:04:36 +0100

    kth largest integer

---
+++ kthLargestInteger.py
@@ -0,0 +1,25 @@
+from ctypes import pointer
+import heapq
+class Solution:
+
+    # Approach 1 using heaps, 
+    # Time complexity n* log n
+    # Space complexity n
+    def kthLargestNumber(self, nums,k):
+
+        heap = []
+        for el in nums:  
+            if len(heap)<k:
+                heapq.heappush(heap, int(el))
+
+            else:
+                heapq.heappush(heap,int(el))
+                heapq.heappop(heap)
+
+        return str(heapq.heappop(heap))
+
+
+ 
+sol = Solution()
+
+print(sol.kthLargestNumber(["2","21","12","1"],3))
