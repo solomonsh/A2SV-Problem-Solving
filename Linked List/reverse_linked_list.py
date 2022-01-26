4c393e815205617def54f03cc96b8a5a380272dc
solomonsh <s.seshiferaw@gmail.com>
2022-01-26 15:51:46 +0100

    reverse_linked_list

---
+++ Linked List/reverse_linked_list.py
@@ -0,0 +1,23 @@
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head):
+         
+            previous = None
+            current = head
+            
+            while current:
+                next_node = current.next
+                
+                current.next = previous
+                
+                previous = current
+                
+                current = next_node
+            
+            return previous
