67834ad9d246358f7734053e9787bbee403d51c2
solomonsh <s.seshiferaw@gmail.com>
2022-01-31 23:10:57 +0100

    adding more list problems

---
+++ Lists/first_last.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def searchRange(self, nums,target):
+        
+        def search(x):
+            low = 0
+            high = len(nums)     
+
+            while low < high:
+                mid = (low + high) // 2
+                if nums[mid] < x:
+                    low = mid+1
+                else:
+                    high = mid    
+
+            return low
+        
+        low = search(target)
+        high = search(target+1)-1
+        
+        if low <= high:
+            return [low, high]
+                
+        return [-1, -1]
+        
+
+sol = Solution()
+
+print(sol.searchRange([7,7,7,7,7],7))
+
+# print(sol.searchRange([1],0))
---
+++ Lists/max_subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+ 
+    def maxSubArray(self, nums):
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+
+        return maxSub
+                     
+
+sol = Solution()
+
+print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4,120]))
