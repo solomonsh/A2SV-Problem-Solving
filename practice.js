5feaf8a9c3907082fcac707450b188c18dab8785
solomonsh <s.seshiferaw@gmail.com>
2022-02-11 17:08:23 +0100

    subArrySumsEqualsK

---
+++ practice.js
@@ -0,0 +1,15 @@
+function showMyName(){
+    console.log('Solomon!')
+
+}
+
+showMyName()
+
+let name = 'sol';
+
+name === "sol"? console.log("yes"): console.log('no')
+
+
+let coolFunction = age => age*12;
+
+console.log(coolFunction(23))
---
+++ queue/subArraySumsEqualsK.py
@@ -0,0 +1,23 @@
+def subarraySum(nums, k):
+
+    sums_val = {0: 1}
+
+    cur_sum = 0
+    count = 0
+
+    for i in range(len(nums)):
+        cur_sum += nums[i]
+
+        if cur_sum-k in sums_val:
+            count += sums_val[cur_sum-k]
+
+        if cur_sum not in sums_val:
+            sums_val[cur_sum] = 1
+        else:
+            sums_val[cur_sum] += 1
+
+    return count
+
+
+print(subarraySum([0, 2], 2))
+# print(subarraySum([0,0,0,0,0,0,0,0,0,0],0))
