51084754d4a86e4ca1335c01de6bfe6fc056072a
solomonsh <s.seshiferaw@gmail.com>
2022-02-17 14:03:00 +0100

    random pick with weight modified

--- Random /randomPickWithWeight.py
+++ Random /randomPickWithWeight.py
@@ -4,11 +4,11 @@ import random
 class Solution:
 
     def __init__(self, w):
-
-        self.weights = [weight/sum(w) for weight in w]
+        self.w = w
         self.indeces = [i for i in range(0, len(w))]
 
     def pickIndex(self) -> int:
 
-        result = random.choices(population=self.indeces,k=1, weights=self.weights)[0]
+        result = random.choices(population=self.indeces,
+                                k=1, weights=self.w)[0]
         return result
