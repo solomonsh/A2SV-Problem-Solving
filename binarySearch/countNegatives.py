e74f9f9eb1e386c94147923470c0f5d340b19e35
solomonsh <s.seshiferaw@gmail.com>
2022-03-04 20:11:29 +0100

    count negative numbers

--- binarySearch/countNegatives.py
+++ binarySearch/countNegatives.py
@@ -25,6 +25,7 @@ class Solution:
 
                 right = mid - 1
 
+    # m*log n time complexty
     def countNegatives(self, grid):
 
         negativeCount = 0
@@ -35,11 +36,28 @@ class Solution:
         return negativeCount
 
 
+    # log m + n 
+    def countNegatives1(self, grid):
+        i = len(grid)-1 
+        j = 0
+        count = 0
+        while i>=0 and j< len(grid[0]):
+            print(i,j)
+            print(grid[i][j])
+            if grid[i][j] < 0:
+                count +=len(grid[0])-j
+                i -= 1
+            else:
+                j +=1
+        return(count)
+
+
 sol = Solution()
-print(sol.countNegatives([[3, -1, -3, -3, -3], [2, -2, -3, -
-      3, -3], [1, -2, -3, -3, -3], [0, -3, -3, -3, -3]]) == 16)
+
 # print(sol.countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]))
 # print(sol.countNegatives([[-12]]))
 
-print(sol.countNegatives([[8, -2, -2, -2, -4, -5, -5], [-2, -3, -3, -4, -4, -5, -5], [-2, -5, -5, -5, -5, -5, -5], [-3, -5, -5, -5, -5, -5, -5],
-      [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5]]))
+# print(sol.countNegatives([[8, -2, -2, -2, -4, -5, -5], [-2, -3, -3, -4, -4, -5, -5], [-2, -5, -5, -5, -5, -5, -5], [-3, -5, -5, -5, -5, -5, -5],
+#       [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5], [-5, -5, -5, -5, -5, -5, -5]]))
+
+print(sol.countNegatives1([[6,5,-4],[4,3,-2],[3,2,1]]))
