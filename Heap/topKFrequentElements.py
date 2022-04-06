be3423f05cd2be06901167c5bf325ee423cad1a6
Solomon Shiferaw <solomon@Solomons-MacBook-Pro.local>
2022-04-06 14:58:10 +0200

    top k frequent elements

--- Heap/topKFrequentElements.py
+++ Heap/topKFrequentElements.py
@@ -2,6 +2,8 @@ from collections import Counter
 import heapq
 
 class Solution:
+    # time complexity n log n
+    # space complexity n+k
     def topKFrequent(self, nums, k):
             num_counter = Counter(nums)
             
@@ -17,6 +19,30 @@ class Solution:
 
             return result
 
+
+    # time complexity n 
+    # space complexity n+k
+    def topKFrequentBucketSort(self, nums, k):
+        num_counter = Counter(nums)
+        counting_sort = [[] for i in range(len(nums)+1)]
+        
+        for i,n in num_counter.items():
+            counting_sort[n].append(i)
+
+        result = []
+        
+        for i in range(len(counting_sort)-1,0,-1): 
+              for n in counting_sort[i]:
+                    result.append(n)
+                    if len(result) == k:
+                      return result
+
+
+            
+        return result 
+
+
+
 sol = Solution()
 
-print(sol.topKFrequent([3,4,2,1,5,5,7],4))
+print(sol.topKFrequentBucketSort([3,4,2,1,5,5,7],4))
